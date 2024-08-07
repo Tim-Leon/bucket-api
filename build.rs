3eed57609866ee017cd9c34eb10986a92d1aeb5c
Tim <tim.leon.persson@gmail.com>
2024-08-07 14:00:18 +0200

    Update

--- build.rs
+++ build.rs
@@ -1,7 +1,15 @@
 fn main() {
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
+    let mut config = tonic_build::configure();
+    #[cfg(feature = "server-api")]
+    {
+        config = config.build_server(true);
+    }
+
+    #[cfg(feature = "client-api")]
+    {
+        config = config.build_client(true);
+    }
+    config
         .compile(
             &[
                 "proto/backend_api.proto",
