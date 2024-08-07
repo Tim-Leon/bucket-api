3f2a25c0934471b58741f119b46e48d96eb3ca80
Tim <tim.leon.persson@gmail.com>
2024-08-07 12:51:02 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-pub mod backend_api {
+pub(crate) mod backend_api {
     //include!(concat!(env!("OUT_DIR"), "/backend_api.rs"));
     tonic::include_proto!("backend_api");
 }
 
-pub mod webhook_event {
+pub(crate) mod webhook_event {
     tonic::include_proto!("webhook_event");
 }
