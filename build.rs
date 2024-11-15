703dc1edcbb229a5d719d671d079497b8b379775
cooki <tim.leon.persson@gmail.com>
2024-11-15 19:34:56 +0000

    update

--- build.rs
+++ build.rs
@@ -12,8 +12,16 @@ fn main() {
     config
         .compile_protos(
             &[
-                "proto/backend_api.proto",
-                "proto/webhook_event.proto",
+                "proto/account/v1/account_service.proto",
+                "proto/storage/v1/storage_service.proto",
+                "proto/common/v1/account.proto",
+                "proto/common/v1/storage.proto",
+                "proto/engine/v1/engine_service.proto",
+                "proto/region/v1/region_service.proto",
+                "proto/search/v1/search_service.proto",
+                "proto/share/v1/share_service.proto",
+                "proto/transcoding/v1/transcoding_service.proto",
+                "proto/webhook/v1/webhook_service.proto",
             ],
             &["proto"],
         )
--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,41 @@ pub mod backend_api {
 pub mod webhook_event {
     tonic::include_proto!("webhook_event");
 }
+
+
+// Error model that is used is: https://cloud.google.com/apis/design/errors
+#[path = "proto/tonic"]
+pub mod tonic {
+    #[cfg(feature = "metrics")]
+    pub use opentelemetry_proto::tonic;
+    #[cfg(feature = "bucket")]
+    #[path = "storage/v1"]
+    pub mod storage {
+
+    }
+    #[cfg(feature = "account")]
+    #[path = "account/v1"]
+    pub mod account {
+
+    }
+    #[cfg(feature = "region")]
+    #[path = "region/v1"]
+    pub mod region {
+
+    }
+    #[cfg(feature = "webhook")]
+    #[path = "webhook/v1"]
+    pub mod webhook {
+
+    }
+    #[cfg(feature = "share")]
+    #[path = "share/v1"]
+    pub mod share {
+
+    }
+    #[cfg(feature = "engine")]
+    #[path = "engine/v1"]
+    pub mod engine  {
+
+    }
+}
